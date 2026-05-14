b26353c99e85d3239c1728ff2100a378037a1f04
ulasuregen <61901077+ulasuregen@users.noreply.github.com>
2026-05-14 20:59:10 +0200

    bla

--- main2.py
+++ main2.py
@@ -1,23 +1,5 @@
-def rotatedDigits(n: int) -> int:
-    valid = 0
-    valid_digits = [2,5,6,9]
-    invalid_digits = [3,4,7]
+a = [5,4,3,7]
 
-    for i in range(1,n+1):
-        valid_B = False
-        while i > 0:
-            print(i)
-            res = i % 10
-            if res in valid_digits:
-                valid_B = True
-            elif res in invalid_digits:
-                valid_B = False
-                break
-            i //= 10
+a.sort()
 
-        valid += valid_B
-
-    return valid
-
-res = rotatedDigits(10)
-print(res)
+print(a)
